1679fd44d8d410f3024b70c5ea73d704a48704f3
hollinwakefield <hollinwakefield@gmail.com>
2020-11-20 13:13:13 -0800

    first commit

---
+++ mail.php
@@ -0,0 +1,18 @@
+<!-- https://mdbootstrap.com/docs/jquery/forms/contact/#antispam -->
+
+<?php
+if(isset( $_POST['name']))
+$name = $_POST['name'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+if(isset( $_POST['message']))
+$message = $_POST['message'];
+if(isset( $_POST['subject']))
+$subject = $_POST['subject'];
+
+$content="From: $name \n Email: $email \n Message: $message";
+$recipient = "hi@quanyunxu.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+echo "Email sent!";
+?> 
